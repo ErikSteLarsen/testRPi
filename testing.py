8a5c1f054745e449d4bd688e2fc24b2d06827c59
Erik Larsen <erikstelarsen@outlook.com>
2022-02-03 23:21:06 +0100

    asd

--- testing.py
+++ testing.py
@@ -1,19 +1,31 @@
+import datetime
+import time
 import pygsheets
-import pandas as pd
+from garo_gm3d import Garo
+
 #authorization
 gc = pygsheets.authorize(service_file='/Users/ErikS/Desktop/Garo-GM3D/test123-337418-2f66f46a491c.json')
-
-# Create empty dataframe
-df = pd.DataFrame()
-
-# Create a column
-df['name'] = ['John', 'Steve', 'Sarah']
-
 #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
 sh = gc.open('test')
-
 #select the first sheet 
 wks = sh[0]
 
-#update the first sheet with df, starting at cell B2. 
-wks.set_dataframe(df,(1,1))
+current_watt_hours = wks.get_value((1,2))
+current_watt_hours = int(current_watt_hours)
+print('Current energy meter standing (as read from the spreadsheet in column B1) is {}Wh'.format(current_watt_hours))
+
+# Initialize the Garo GM3D
+garo = Garo(17, current_watt_hours)
+garo.listen()
+
+counter = 2
+
+
+while True:
+    
+    current_watt_hours = garo.get_watt_hours()
+    
+    wks.update_value((counter,1), str(datetime.datetime.now()))
+    wks.update_value((counter,2), "test")
+    counter += 1
+    time.sleep(30)
